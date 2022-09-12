dddb9ccc4fd0d6a4a3469405a05f259330c8f139
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-09-12 12:32:08 +0200

    Started working on project, files created, basic layout complete

---
+++ app.js
@@ -0,0 +1,38 @@
+const dummyData = require('./dummy-data')
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine(
+  'hbs',
+  expressHandlebars.engine({
+    defaultLayout: 'main.hbs',
+  }),
+)
+
+app.get('/', function (request, response) {
+  const model = {
+    humans: dummyData.humans,
+  }
+  response.render('home.hbs', model)
+})
+app.get('/views/home.hbs', function (request, response) {
+  const model = {
+    humans: dummyData.humans,
+   }
+  response.render('home.hbs', model)
+})
+app.get('/views/about.hbs', function (request, response) {
+  const model = {
+    humans: dummyData.humans,
+  }
+  response.render('about.hbs', model)
+})
+app.get('/views/contact.hbs', function (request, response) {
+  const model = {
+    humans: dummyData.humans,
+  }
+  response.render('contact.hbs', model)
+})
+app.listen(8080)
---
+++ dummy-data.js
@@ -0,0 +1,13 @@
+exports.human = [{
+    id: 0, 
+    name: "Alice"
+}, {
+    id: 1,
+    name: "Bob"
+}]
+
+exports.pets = [{
+    id:0,
+    humanId: 1,
+    name: "Catty"
+}]
