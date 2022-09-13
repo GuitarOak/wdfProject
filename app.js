4a96ca31f61f2c2066246e1a302e663847834786
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-09-13 20:45:46 +0200

    Started working on db, cant display data. but is retrieving

--- app.js
+++ app.js
@@ -4,18 +4,71 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('database.db', function(error){
+  if (error) {
+    console.error(error.message)
+  }else{
+   console.log('Connected to the database.')
+  }
+})
+
+db.run("CREATE TABLE IF NOT EXISTS  'Comments' ( 'Id'	INTEGER, 'PostId'	INTEGER, 'Comment'	TEXT NOT NULL, FOREIGN KEY('PostId') REFERENCES 'Posts'('Id'), PRIMARY KEY('Id' AUTOINCREMENT) ) ", function(error){
+  if(error){
+    console.log(error)
+  }else{
+    console.log("Comments table succesfully created if not existed")
+  }
+})
+db.run("CREATE TABLE IF NOT EXISTS  'Posts' ( 'Id'	INTEGER,'Text'	TEXT NOT NULL, PRIMARY KEY('Id' AUTOINCREMENT) ) ", function(error){
+  if(error){
+    console.log(error)
+  }else{
+    console.log("Posts table succesfully created if not existed")
+  }
+})
+
+/*
+Recieving all Posts from DB
+*/
+let allPosts;
+const selectAllPostsQuery = "SELECT Text FROM Posts"
+db.all(selectAllPostsQuery, function(error, posts){
+  if(error){
+    console.log(error)
+  }else{
+    console.log("Selected Text from posts")
+    allPosts = posts;
+  }
+})
+
+/*
+Recieving all Comments from DB
+*/
+let allComments;
+const selectAllCommentsQuery = "SELECT * FROM Comments"
+db.all(selectAllCommentsQuery, function(error, comments){
+  if (error) {
+    console.log(error)
+  }else{
+    console.log("Selected * from comments")
+    allComments = comments
+  }
+})
+
+app.use(express.static('public'))
+
 app.engine(
   'hbs',
   expressHandlebars.engine({
     defaultLayout: 'main.hbs',
   }),
 )
-app.use(express.static('public'))
 
 app.get('/', function (request, response) {
   const model = {
     humans: dummyData.humans,
-    posts: dummyData.posts,
+    posts: allPosts,
     comments: dummyData.postComments,
   }
   response.render('home.hbs', model)
@@ -23,28 +76,22 @@ app.get('/', function (request, response) {
 app.get('/views/home.hbs', function (request, response) {
   const model = {
     humans: dummyData.humans,
-    posts: dummyData.posts,
+    posts: allPosts,
     comments: dummyData.postComments,
   }
   response.render('home.hbs', model)
 })
 app.get('/views/about.hbs', function (request, response) {
-  const model = {
-    humans: dummyData.humans,
-  }
-  response.render('about.hbs', model)
+ 
+  response.render('about.hbs')
 })
 app.get('/views/contact.hbs', function (request, response) {
-  const model = {
-    humans: dummyData.humans,
-  }
-  response.render('contact.hbs', model)
+
+  response.render('contact.hbs')
 })
 app.get('/views/login.hbs', function (request, response) {
-  const model = {
-    humans: dummyData.humans,
-  }
-  response.render('login.hbs', model)
+
+  response.render('login.hbs')
 })
 app.get('/views/admin.hbs', function (request, response) {
   const model = {
