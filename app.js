59679bd5373c606b8efcd08dca384fcdf2bdacbb
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-10-03 21:39:23 +0200

    progress, fr this time

--- app.js
+++ app.js
@@ -5,37 +5,38 @@ const expressHandlebars = require('express-handlebars')
 const app = express()
 
 const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database('database.db', function(error){
+const db = new sqlite3.Database('database.db', function (error) {
   if (error) {
     console.error(error.message)
-  }else{
-   console.log('Connected to the database.')
+  } else {
+    console.log('Connected to the database.')
   }
 })
 
-db.run("CREATE TABLE IF NOT EXISTS  'Comments' ( 'Id'	INTEGER, 'PostId'	INTEGER, 'Comment'	TEXT NOT NULL, FOREIGN KEY('PostId') REFERENCES 'Posts'('Id'), PRIMARY KEY('Id' AUTOINCREMENT) ) ", function(error){
-  if(error){
-    console.log(error)
-  }else{
-    console.log("Comments table succesfully created if not existed")
+db.run(
+  "CREATE TABLE IF NOT EXISTS  'Comments' ( 'Id'	INTEGER, 'PostId'	INTEGER, 'Comment'	TEXT NOT NULL, FOREIGN KEY('PostId') REFERENCES 'Posts'('Id'), PRIMARY KEY('Id' AUTOINCREMENT) ) ",
+  function (error) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Comments table succesfully created if not existed')
+    }
   }
-})
-db.run("CREATE TABLE IF NOT EXISTS  'Posts' ( 'Id'	INTEGER,'Text'	TEXT NOT NULL, PRIMARY KEY('Id' AUTOINCREMENT) ) ", function(error){
-  if(error){
-    console.log(error)
-  }else{
-    console.log("Posts table succesfully created if not existed")
+)
+db.run(
+  "CREATE TABLE IF NOT EXISTS  'Posts' ( 'Id'	INTEGER,  'Text'	TEXT NOT NULL, PRIMARY KEY('Id' AUTOINCREMENT) ) ",
+  function (error) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Posts table succesfully created if not existed')
+    }
   }
-})
+)
 
 /*
 Recieving all Posts + Comments from DB
 */
-let postModel = {
-  text: '',
-  comment: [''],
-};
-let allPosts = [postModel]
 
 app.use(express.static('public'))
 
@@ -43,58 +44,54 @@ app.engine(
   'hbs',
   expressHandlebars.engine({
     defaultLayout: 'main.hbs',
-  }),
+  })
 )
 
 app.get('/', function (request, response) {
-  
-const selectAllPostsQuery = "SELECT * FROM Posts"
-db.all(selectAllPostsQuery, async function(error, posts){
-  if(error){
-    console.log(error)
-  }else{
-    console.log("Selected all from posts", posts)
-   
-    posts.forEach(async post => {
-      postModel.text = post.Text
-      console.log("Post,text: ", post.Text)
+  const selectAllPostsQuery = 'SELECT * FROM Posts'
 
-      const selectCommentForPost = "SELECT Comment FROM Comments WHERE PostID = ?"
-      
-      db.all(selectCommentForPost,post.Id, function(error,comments){
-        if(error){
-          console.log(error)
-        }else{
-          console.log("Selected comments for posts: )", comments)
-          postModel.comment = comments
-          allPosts.push("Postmodel: " , postModel)
-        }        
+  db.all(selectAllPostsQuery, function (error, posts) {
+    if (error) {
+      console.log(error)
+    } else {
+      const getCommentsByPostId = new Promise((resolve, reject) => {
+        const allPosts = []
+        posts.forEach((post) => {
+          const selectCommentForPost =
+            'SELECT Comment FROM Comments WHERE PostID = ?'
+
+          db.all(selectCommentForPost, post.Id, function (error, comments) {
+            if (error) {
+              console.log(error)
+            } else {
+              const text = post.Text
+              allPosts.push({ text, comments })
+            }
+          })
+        })
+        resolve(allPosts)
       })
-    });
-
-  console.log("Allposts: ", allPosts)
-  
-  const model = {
-    posts: allPosts
-  }
-  response.render('home.hbs', model)
-  }
+      getCommentsByPostId.then((allPosts) => {
+        console.log('test allPosts', allPosts)
+        const model = {
+          posts: allPosts,
+        }
+        response.render('home.hbs', model)
+      })
+    }
+  })
 })
-})
 
-app.get('/views/about.hbs', function (request, response) {
- 
+app.get('/about', function (request, response) {
   response.render('about.hbs')
 })
-app.get('/views/contact.hbs', function (request, response) {
-
+app.get('/contact', function (request, response) {
   response.render('contact.hbs')
 })
-app.get('/views/login.hbs', function (request, response) {
-
+app.get('/login', function (request, response) {
   response.render('login.hbs')
 })
-app.get('/views/admin.hbs', function (request, response) {
+app.get('/admin', function (request, response) {
   const model = {
     humans: dummyData.humans,
     posts: dummyData.posts,
