4845d4fba7fe01d2c15784b9e9c316af0f9cf4a6
Emil Karlsson <emil.h.karlsson@gmail.com>
2023-01-03 17:02:03 +0100

    Added third resource, CRUD works perfectly

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const parseForm = bodyParser.urlencoded({ extended: false })
 const sqlite3 = require('sqlite3')
 const bcrypt = require('bcrypt')
-const saltRounds = 10
 
 const app = express()
 app.use(session({
@@ -28,7 +27,6 @@ const db = new sqlite3.Database('database.db', function (error) {
     const model = {
       error: 'Database error'
     }
-    response.render('error.hbs', model)
   } else {
     console.log('Connected to Database')
   }
@@ -40,19 +38,27 @@ db.run(
       const model = {
         error: 'Database error'
       }
-      response.render('error.hbs', model)
     }
   },
 )
 db.run(
-  "CREATE TABLE IF NOT EXISTS  'Posts' ( 'Id'	INTEGER,  'Text'	TEXT NOT NULL, PRIMARY KEY('Id' AUTOINCREMENT) ) ",
+  "CREATE TABLE IF NOT EXISTS 'MyMusic' ( 'ID'	INTEGER, 'Title'	TEXT, 'Link'	TEXT, 'ImageLink'	TEXT, PRIMARY KEY('ID' AUTOINCREMENT)  );",
+  function (error) {
+    if (error) {
+      const model = {
+        error: 'Database error'
+      }
+    }
+  }
+)
+db.run(
+  "CREATE TABLE IF NOT EXISTS 'Posts' ( 'Id'	INTEGER,  'Text'	TEXT NOT NULL, PRIMARY KEY('Id' AUTOINCREMENT) ) ",
   function (error) {
     if (error) {
       console.log("Error", error)
       const model = {
         error: 'Database error'
       }
-      response.render('error.hbs', model)
     }
   },
 )
@@ -108,7 +114,7 @@ app.post('/', parseForm, function (request, response) {
   const comment = request.body.commentInput
   if (comment.length < MIN_COMMENT_LENGTH) {
     const model = {
-      error: 'Comment needs to be longer than 5 characters'
+      error: 'Comment needs to be longer than ' + MIN_COMMENT_LENGTH + ' characters'
     }
     response.render('error.hbs', model)
   } else {
@@ -116,7 +122,7 @@ app.post('/', parseForm, function (request, response) {
     const commentValues = [comment, postID]
     const insertCommentQuery =
       'INSERT INTO Comments (Comment, PostId) VALUES (?, ?)'
-    db.all(insertCommentQuery, commentValues, function (error, cb) {
+    db.run(insertCommentQuery, commentValues, function (error, callback) {
       if (error) {
         const model = {
           error: 'Database error'
@@ -127,6 +133,26 @@ app.post('/', parseForm, function (request, response) {
       }
     })
   }
+})
+
+
+app.get('/my-music', function (request, response) {
+  const selectAllMyMusicQuery = 'SELECT * FROM MyMusic'
+  db.all(selectAllMyMusicQuery, function (error, myMusic) {
+    if (error) {
+      const model = {
+        error: 'Database error'
+      }
+      response.render('error.hbs', model)
+    } else {
+      const model = {
+        myMusic
+      }
+      console.log(myMusic)
+      console.log(myMusic.length)
+      response.render('myMusic.hbs', model)
+    }
+  })
 })
 
 app.get('/about', function (request, response) {
@@ -140,13 +166,12 @@ app.get('/login', function (request, response) {
 })
 
 const adminEmail = 'admin@admin.com'
-//Password = Admin123
-const adminPassword = '$2b$10$wHdoX38LnOEj4eQePoPj7eNTui3VzUPyximWVcwE672Pb7YUPyWPK'
+const adminHashedPassword = '$2b$10$wHdoX38LnOEj4eQePoPj7eNTui3VzUPyximWVcwE672Pb7YUPyWPK'
 
 app.post('/authenticate-login', parseForm, function (request, response) {
   const email = request.body.emailInput
   const password = request.body.passwordInput
-  const validatedPassword = bcrypt.compareSync(password, adminPassword)
+  const validatedPassword = bcrypt.compareSync(password, adminHashedPassword)
 
   if (email == adminEmail && validatedPassword) {
     request.session.isLoggedIn = true
@@ -161,6 +186,19 @@ app.post('/authenticate-login', parseForm, function (request, response) {
 
 app.get('/admin', function (request, response) {
   if (request.session.isLoggedIn) {
+    var music
+    const selectAllMyMusicQuery = 'SELECT * FROM MyMusic'
+    db.all(selectAllMyMusicQuery, function (error, myMusic) {
+      if (error) {
+        const model = {
+          error: 'Database error'
+        }
+        response.render('error.hbs', model)
+      } else {
+        music=myMusic
+      }
+    })
+
     const selectAllPostsQuery = 'SELECT * FROM Posts'
     db.all(selectAllPostsQuery, function (error, posts) {
       if (error) {
@@ -171,6 +209,7 @@ app.get('/admin', function (request, response) {
       } else {
         const getCommentsByPostId = new Promise((resolve, reject) => {
           const allPosts = []
+      
           posts.forEach((post) => {
             const selectCommentForPost =
               'SELECT * FROM Comments WHERE PostID = ?'
@@ -192,7 +231,9 @@ app.get('/admin', function (request, response) {
         getCommentsByPostId.then((allPosts) => {
           const model = {
             posts: allPosts,
+            music
           }
+          console.log(music)
           response.render('admin.hbs', model)
         })
       }
@@ -208,7 +249,7 @@ app.post('/remove-post', parseForm, function (request, response) {
   if (request.session.isLoggedIn) {
     const postId = request.body.postId
     const removePostQuery = 'DELETE FROM Posts WHERE Id = ?'
-    db.all(removePostQuery, postId, function (error, cb) {
+    db.run(removePostQuery, postId, function (error, callback) {
       if (error) {
         const model = {
           error: 'Database error'
@@ -231,13 +272,13 @@ app.post('/update-post', parseForm, function (request, response) {
     const postText = request.body.postText
     if (postText.length < MIN_POST_LENGTH) {
       const model = {
-        error: 'Post needs to be longer than 10 characters'
+        error: 'Post needs to be longer than ' + MIN_POST_LENGTH + ' characters'
       }
       response.render('error.hbs', model)
     } else {
       const updatedPostValues = [postText, postId]
       const updatePostQuery = 'Update Posts SET Text = ? WHERE Id = ?'
-      db.all(updatePostQuery, updatedPostValues, function (error, cb) {
+      db.run(updatePostQuery, updatedPostValues, function (error, callback) {
         if (error) {
           const model = {
             error: 'Database error'
@@ -255,17 +296,69 @@ app.post('/update-post', parseForm, function (request, response) {
     response.render('error.hbs', model)
   }
 })
+app.post('/remove-music', parseForm, function(request, response){
+  if (request.session.isLoggedIn) {
+    const musicID = request.body.musicID
+    const removePostQuery = 'DELETE FROM MyMusic WHERE ID = ?'
+    db.run(removePostQuery, musicID, function (error, callback) {
+      if (error) {
+        const model = {
+          error: 'Database error'
+        }
+        response.render('error.hbs', model)
+      } else {
+        response.redirect('/admin')
+      }
+    })
+  } else {
+    const model = {
+      error: 'You dont have permission to access to this'
+    }
+    response.render('error.hbs', model)
+  }
+} ) 
+app.post('/update-music', parseForm, function(request, response){
+  if (request.session.isLoggedIn) {
+    const musicID = request.body.musicID
+    const newMusicTitle = request.body.musicTitle
+    if (newMusicTitle.length < 3) {
+      const model = {
+        error: 'Post needs to be longer than 3 characters'
+      }
+      response.render('error.hbs', model)
+    } else {
+      const updatedMusicValues = [newMusicTitle, musicID]
+      const updateMusicQuery = 'Update MyMusic SET Title = ? WHERE ID = ?'
+      db.run(updateMusicQuery, updatedMusicValues, function (error, callback) {
+        if (error) {
+          const model = {
+            error: 'Database error'
+          }
+          response.render('error.hbs', model)
+        } else {
+          response.redirect('/admin')
+        }
+      })
+    }
+  } else {
+    const model = {
+      error: 'You dont have permission to access this'
+    }
+    response.render('error.hbs', model)
+  }
+})
+
 app.post('/add-post', parseForm, function (request, response) {
   if (request.session.isLoggedIn) {
     const postInput = request.body.postInput
     if (postInput.length < MIN_POST_LENGTH) {
       const model = {
-        error: 'Post needs to be longer than 10 characters'
+        error: 'Post needs to be longer than ' + MIN_POST_LENGTH + ' characters'
       }
       response.render('error.hbs', model)
     } else {
       const addPostQuery = 'INSERT INTO Posts (Text) VALUES (?)'
-      db.all(addPostQuery, postInput, function (error, cb) {
+      db.run(addPostQuery, postInput, function (error, callback) {
         if (error) {
           const model = {
             error: 'Database error'
@@ -283,11 +376,48 @@ app.post('/add-post', parseForm, function (request, response) {
     response.render('error.hbs', model)
   }
 })
+app.post('/add-music', parseForm, function(request,response){
+  if (request.session.isLoggedIn) {
+    const title = request.body.newMusicTitle
+    const link = request.body.newMusicLink
+    const imageLink = request.body.newMusicImageLink
+    newMusicValues = [title, link, imageLink]
+    if (title.length < 3) {
+      const model = {
+        error: 'Title needs to be longer than 3 characters'
+      }
+      response.render('error.hbs', model)
+    } else if(link.length < 5 || imageLink.length < 5) {
+      const model = {
+        error: 'Please provide a valid link'
+      }
+      response.render('error.hbs', model)
+    } else {
+      const addMusicQuery = 'INSERT INTO MyMusic (Title, Link, ImageLink) VALUES (?,?,?)'
+      db.run(addMusicQuery, newMusicValues, function (error, callback) {
+        if (error) {
+          const model = {
+            error: 'Database error'
+          }
+          response.render('error.hbs', model)
+        } else {
+          response.redirect('/admin')
+        }
+      })
+    }
+  } else {
+    const model = {
+      error: 'You dont have permission to access this'
+    }
+    response.render('error.hbs', model)
+  }
+})
+
 app.post('/remove-comment', parseForm, function (request, response) {
   if (request.session.isLoggedIn) {
     const commentId = request.body.commentId
     const removeCommentQuery = 'DELETE FROM Comments WHERE Id = ?'
-    db.all(removeCommentQuery, commentId, function (error, cb) {
+    db.run(removeCommentQuery, commentId, function (error, callback) {
       if (error) {
         const model = {
           error: 'Database error'
@@ -308,9 +438,10 @@ app.post('/logout', function (request, response) {
   request.session.isLoggedIn = false
   response.redirect('/')
 })
-app.get('/error', function () {
+app.get('/error', function (request, response) {
   response.render('error.hbs')
 })
+
 let port = process.env.PORT
 if (port == null || port == "") {
   port = 8000
